031b27689ef3d2c35bf1e3acb5b8397df0e71e10
Codux <codux@codux.com>
2024-06-28 11:57:42 +0530

    a

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,15 +8,11 @@ export interface HeaderProps {
     className?: string;
 }
 
-/**
- * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
- */
 export const Header = ({ className }: HeaderProps) => {
     return (
         <div className={classNames(styles.root, className)}>
             <a href="/" className={About_module.title}>
-                <CoduxSvg />
+                <CoduxSvg width="100px" height="100px" />
             </a>
             <div className={styles.menu}>
                 <NavLink
