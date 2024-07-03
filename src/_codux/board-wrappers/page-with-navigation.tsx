8e23afc84212fdec4a766a3fa063d293ea21b03f
Codux <codux@codux.com>
2024-07-03 16:33:48 +0530

    even more deltes

--- src/_codux/board-wrappers/page-with-navigation.tsx
+++
@@ -1,7 +0,0 @@
-import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-import { routes } from '../../routes';
-
-export function PagewithNavigation(props: { path?: string }) {
-    const router = createMemoryRouter(routes, { initialEntries: [props.path ?? '/'] });
-    return <RouterProvider router={router} />;
-}
